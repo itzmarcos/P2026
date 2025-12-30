54f4618b0817fd5cb7bddca56348338578c8dc2e
itzmarcos <marcozfuwa@gmail.com>
2025-12-30 17:45:32 -0300

    04

--- 04.py
+++ 04.py
@@ -1,11 +1,17 @@
 # Análise de dados em uma Tupla
-cont = 0
-a = int(input('Digite um numero: '))
-b = int(input('Digite outro numero: '))
-c = int(input('Digite mais um numero: '))
-d = int(input('Digite o ultimo numero: '))
-cont += 1
-numeros = (a, b, c, d)
-print(f'Voce digitou os valores {numeros}')
-for pos, i in enumerate(numeros):
-    print(f'O valor ')
+
+num = (int(input('Digite um numero: ')),
+       int(input('Digite um numero: ')),
+       int(input('Digite um numero: ')),
+       int(input('Digite um numero: ')))
+
+print(f'Voce digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1} posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitados foram ', end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
