4eca6d5515bad99022d5de13b8f49d5c1d36c6c0
itzmarcos <marcozfuwa@gmail.com>
2026-01-06 07:26:32 -0300

    07

--- 07.py
+++ 07.py
@@ -3,15 +3,19 @@ valores = list()
 for c in range(0, 3):
     valores.append(int(input(f'Digite um valor na Posição {c}: ')))
 
-if c == 0:
-    maior = menor = valores[c]
-else:
-    if valores[c] > maior:
-        maior = valores[c]
-    if valores[c] < menor:
-        menor = valores[c]
-
+    if c == 0:
+        maior = menor = valores[c]
+    else:
+        if valores[c] > maior:
+            maior = valores[c]
+        if valores[c] < menor:
+            menor = valores[c]
 
 print(f'Voce digitou os valores {valores}')
-print(f'O maior valor digita foi {maior}')
-print(f'O menor valor digitado foi {menor}')
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f'O maior valor digita foi {maior} e apareceu nas posiçoes {i}')
+
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f'O menor valor digitado foi {menor} e apareceu na posições {i}')
