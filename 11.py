8c800b943a68b5b64b6ed621bc6fca86563ea383
itzmarcos <marcozfuwa@gmail.com>
2026-01-07 18:01:13 -0300

    12

--- 11.py
+++ 11.py
@@ -18,3 +18,22 @@ while True:
 print(f'A lista completa é: {lista}')
 print(f'A lista de pares é: {pares}')
 print(f'A lista de impares é: {impares}')
+
+
+# OUTRA SOLUÇÃO
+
+
+# while True:
+#     lista.append(int(input('Digite um numero: ')))
+#     resp = str(input('Quer continua: '))
+#     if resp in 'Nn':
+#         break
+# for i, v in enumerate(lista):
+#     if v % 2 == 0:
+#         pares.append(v)
+#     elif v % 2 == 1:
+#         impares.append(v)
+
+# print(f'A lista completa é {lista}')
+# print(f'A lista de pares é {pares}')
+# print(f'A lista de impares é {impares}')
