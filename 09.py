6c1ea9bda0a765f044787937ee7b251715fbf061
itzmarcos <marcozfuwa@gmail.com>
2026-01-07 07:36:15 -0300

    10

--- 09.py
+++ 09.py
@@ -1,16 +1,17 @@
 # Lista ordenada sem repetições
 
 valores = list()
-
-for v in range(3):
+for v in range(0, 3):
     valor = int(input('Digite um valor: '))
-    if valor in valores:
+    if v == 0 or valor > valores[-1]:
         valores.append(valor)
-        print('Adicionado no final da lista!')
-
-
-
-
-
-valores.sort()
+        print('Adicionado no final da lista')
+    else:
+        pos = 0
+        while pos < len(valores):
+            if valor <= valores[pos]:
+                valores.insert(pos, valor)
+                print(f'Adicionado na posição {pos} da lista')
+                break
+            pos += 1
 print(f'Os valores digitados foram: {valores}')
---
+++ 10.py
@@ -0,0 +1,19 @@
+# Extraindo dados de uma Lista
+cont = 0
+lista = []
+while True:
+    lista.append(int(input('Digite um valor: ')))      
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continua: ')).strip().upper()
+    if resp == 'N':
+        break
+
+
+lista.sort(reverse=True)
+print(f'Voce digitou {len(lista)} elementos')
+print(f'Os valores em ordem decrescente foram: {lista}')
+if 5 in lista:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não faz parte da lista!')
