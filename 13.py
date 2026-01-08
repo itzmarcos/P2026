174295c65119e304a1cd4cce3c60b573e2e89b57
itzmarcos <marcozfuwa@gmail.com>
2026-01-08 17:17:21 -0300

    13

--- 13.py
+++ 13.py
@@ -2,26 +2,32 @@
 
 pessoas = []
 dados = []
-total = maior = menor = 0
+maior = menor = 0
 while True:
     pessoas.append(str(input('Nome: ')))
-    pessoas.append(int(input('Peso: ')))
-    total += 1
-    pessoas.append(dados[:])
-    dados.clear()
-    if pessoas[1] == 1:
+    pessoas.append(float(input('Peso: ')))
+    if len(dados) == 0:
         maior = menor = pessoas[1]
     else:
         if pessoas[1] > maior:
             maior = pessoas[1]
         if pessoas[1] < menor:
             menor = pessoas[1]
-
+    dados.append(pessoas[:])
+    pessoas.clear()
     resp = ' '
     while resp not in 'SN':
-        resp = str(input('Quer continua: ')).strip().upper()
+        resp = str(input('Quer continua: ')).strip().upper()[0]
     if resp == 'N':
         break
-
-print(f'O total de pessoas cadastradas foram {total}')
-print(f'O maior peso é de {maior}')
+print(f'O total de pessoas cadastradas foram {len(dados)}.')
+print(f'O maior peso foi {maior}KG', end=' ')
+for p in dados:
+    if p[1] == maior:
+        print(f'{p[0]}', end=' ')
+print()
+print(f'O menor peso foi {menor}KG', end=' ')
+for p in dados:
+    if p[1] == menor:
+        print(f'{p[0]}', end=' ')
+print()
