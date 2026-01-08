960203198bfe6c56ee6b7f626db9da81208d0871
itzmarcos <marcozfuwa@gmail.com>
2026-01-08 17:36:42 -0300

    corrigindo

--- 14.py
+++ 14.py
@@ -1,1 +1,12 @@
 # Listas com pares e ímpares
+valores = []
+for v in range(1, 8):
+    valor = (int(input(f'Digite o {v} valor: ')))
+    valores.append(valor)
+    if valor % 2 == 0:
+        valores.append(valor)
+    if valor % 2 == 1:
+        valores.append(valor)
+
+
+print(valores)
