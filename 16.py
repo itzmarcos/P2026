4c3657eebe752c659f8ed8e0034121ac026c74c0
itzmarcos <marcozfuwa@gmail.com>
2026-01-09 07:26:08 -0300

    corrgindo

---
+++ 16.py
@@ -0,0 +1,15 @@
+# MATRIZ
+soma = cont = 0
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+if matriz[l][c] % 2 == 0:
+    soma += matriz[l][c]
+
+
+print(f'A soma dos valores pares é: {soma}')
