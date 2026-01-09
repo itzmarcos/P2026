8d92d9f532db140fe2c5f97aaf293826df3abed9
itzmarcos <marcozfuwa@gmail.com>
2026-01-09 07:57:41 -0300

    corrigido com sucesso

--- 16.py
+++ 16.py
@@ -1,5 +1,5 @@
 # MATRIZ
-soma = cont = 0
+par = col = maior = 0
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
     for c in range(0, 3):
@@ -7,9 +7,14 @@ for l in range(0, 3):
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            par += matriz[l][c]
     print()
-if matriz[l][c] % 2 == 0:
-    soma += matriz[l][c]
-
-
-print(f'A soma dos valores pares é: {soma}')
+print(f'A soma dos valores pares é: {par}')
+for l in range(0, 3):
+    col += matriz[l][2]
+print(f'A soma dos valores da coluna 3 é {col}')
+for c in range(0, 3):
+    if matriz[1][c] > maior:
+        maior = matriz[1][c]
+print(f'O maior numero digita na linha dois é: {maior}')
