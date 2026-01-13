7e236668491b6988ebe74dc17e027a3921ec7b59
itzmarcos <marcozfuwa@gmail.com>
2026-01-13 17:46:46 -0300

    21

--- 21.py
+++ 21.py
@@ -1,12 +1,15 @@
 # Cadastro de Trabalhador em Python
+from datetime import datetime
+data = datetime.now().year
 dados = dict()
 
 dados['nome'] = str(input('Nome: '))
-dados['ano'] = int(input('Ano de Nascimento: '))
+dados['idade'] = data - int(input('Ano de Nascimento: '))
 dados['carteira'] = int(input('Carteira de Trabalho (0 não tem): '))
 if dados['carteira'] != 0:
     dados['contrato'] = int(input('Ano de Contratação: '))
-    dados['salario'] = int(input('Salario: '))
+    dados['salario'] = int(input('Salario: R$'))
+    dados['aposentadoria'] = dados['idade'] + 35
 
-
-print(dados)
+for k, v in dados.items():
+    print(f'- {k} tem o valor {v}')
