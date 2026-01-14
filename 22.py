ba66c4dba6e28e05d7c0e0dcab6c04cd4319237e
itzmarcos <marcozfuwa@gmail.com>
2026-01-14 17:15:32 -0300

    22

--- 22.py
+++ 22.py
@@ -1,11 +1,19 @@
 # Cadastro de Jogador de Futebol
 dados = dict()
 gols = list()
-
+soma = 0
 dados['nome'] = str(input('Nome do jogador: '))
 partidas = int(input(f'Quantas partidas {dados["nome"]} jogou: '))
+dados['gols'] = gols
 for p in range(0, partidas):
-    dados['gols'] = int(input(f'Quantos gols na partida {p}: '))
-    gols.append(dados['gols'])
-
-print(gols)
+    gol = (int(input(f'Quantos gols na partida {p}: ')))
+    soma += gol
+    gols.append(gol)
+    dados['total'] = soma    
+print(dados)
+for k, v in dados.items():
+    print(f'O campo {k} tem o valor {v}')
+print(f'O jogador {dados["nome"]} jogou {partidas} partidas.')
+for p, g in enumerate(gols):
+    print(f'=> Na partida {p}, fez {g} gols.')
+print(f'Foi um total de {dados["total"]} gols')
