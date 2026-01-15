3f262bafe38863adc539d45bd5e7620fdba53f03
itzmarcos <marcozfuwa@gmail.com>
2026-01-15 07:40:54 -0300

    corrigindo

--- 23.py
+++ 23.py
@@ -3,10 +3,15 @@ dados = dict()
 info = list()
 while True:
     nome = (str(input('Nome: ')))
-    info.append(nome)       
-    while sexo not in 'MF':
-        sexo = ' '
+    info.append(nome)     
+    sexo = ' '  
+    while sexo not in 'MF':   
         sexo = (str(input('Sexo[M/F]: '))).strip().upper()[0]
+    if sexo == 'MF':
         info.append(sexo)
-    if sexo != 'MF':
-        print('ERRO! Por favor, digite apenas M ou F.')
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar: ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(info)
