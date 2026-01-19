04d086e9afccb5a0b754ee55e5e4f3ed71ffceeb
itzmarcos <marcozfuwa@gmail.com>
2026-01-19 19:03:50 -0300

    25

---
+++ 25.py
@@ -0,0 +1,11 @@
+#
+def area(lar, comp):
+    resultado = lar * comp
+    print(f'A área de um terreno de {lar} x {comp} é de {resultado}m²')
+
+
+l = float(input('Largura(m): '))
+c = float(input('Comprimento(m): '))
+
+area(l, c)
+
