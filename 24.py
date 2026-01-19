8138acb5865617ce5efb45c989790a64e7802741
itzmarcos <marcozfuwa@gmail.com>
2026-01-19 07:42:12 -0300

    all

--- 24.py
+++ 24.py
@@ -1,12 +1,45 @@
 # Aprimorando os Dicionários
-jogadores = dict()
-lista = list()
+jogadores = list()
+jogador = dict()
+partidas = list()
+
 while True:
-    jogadores['nome'] = str(input('Nome do jogador: '))
-    partidas = int(input(f'Quantas partidas {jogadores["nome"]} jogou: '))
-    for p in range(0, partidas):
-        gol = int(input(f'Quantos gols na partida {p}: '))
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou: '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'  Quantos gols na partida {c+1}: ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    jogadores.append(jogador.copy())
     while True:
         resp = str(input('Quer continuar: ')).upper()[0]
-        if resp in 'Nn':
+        if resp in 'SN':
             break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('=' * 30)
+print('COD ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-' * 40)
+for k, v in enumerate(jogadores):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
+while True:
+    busca = int(input('Mostrar os dados de qual jogador: [999] STOP'))
+    if busca == 999:
+        break
+    if busca >= len(jogadores):
+        print(f'Erro! não existe jogador com codigo {busca}!')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {jogadores[busca]["nome"]:}')
+        for i, g in enumerate(jogadores[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+print('PROGRAMA FINALIZADO')
