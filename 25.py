2546575f2c94a1c765d0b9b21824a1888192abe4
itzmarcos <marcozfuwa@gmail.com>
2026-01-19 19:09:55 -0300

    nao terminado

--- 25.py
+++ 25.py
@@ -1,4 +1,4 @@
-#
+# Função que calcula área
 def area(lar, comp):
     resultado = lar * comp
     print(f'A área de um terreno de {lar} x {comp} é de {resultado}m²')
---
+++ 26.py
@@ -0,0 +1,9 @@
+#
+def escreva():
+    print('-' * len(n))
+    print(f'{n}')
+    print('-' * len(n))
+
+n = str(input('Escreva: '))
+
+escreva()
