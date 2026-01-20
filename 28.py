1bd177735189f63fe50a5e23bb3ca334d041775e
itzmarcos <marcozfuwa@gmail.com>
2026-01-20 18:07:11 -0300

    28

---
+++ 28.py
@@ -0,0 +1,10 @@
+# Função que descobre o maior
+import random
+
+def valores(* num):
+    
+    print('Analisando os valores passados...')
+    print(f'{num} Foram informados {len(num)} valores ao todo')
+
+valores(2, 9, 4, 5, 7, 1)
+valores(4, 7, 0)
