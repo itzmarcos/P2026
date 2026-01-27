4689690057fedaffbe0ac80e8cd8e9a73319b0e5
itzmarcos <marcozfuwa@gmail.com>
2026-01-27 11:55:23 -0300

    corrigindo

---
+++ 33.py
@@ -0,0 +1,11 @@
+#
+def leiaInt():
+    if leiaInt == ' ':
+        print('ERRO! Digite um número inteiro válido.')
+    elif leiaInt == '':
+        print('ERRO! Digite um número inteiro válido.')
+    return input(n)
+leiaInt()
+
+n = leiaInt('Digite um número: ')
+print(f'Voce acabou de digitar o número {n}')
