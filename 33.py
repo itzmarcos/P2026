fe628ff93c98b7d3c226fe1a93d55ff96765ea7f
itzmarcos <marcozfuwa@gmail.com>
2026-01-27 12:14:24 -0300

    corrigido

--- 33.py
+++ 33.py
@@ -1,11 +1,11 @@
-#
-def leiaInt():
-    if leiaInt == ' ':
-        print('ERRO! Digite um número inteiro válido.')
-    elif leiaInt == '':
-        print('ERRO! Digite um número inteiro válido.')
-    return input(n)
-leiaInt()
+# Validando entrada de dados em Python
+def leiaInt(mensagem):
+    while True:
+        valor = input(mensagem)
+        if valor.isdigit():
+            return int(valor)
+        else:
+            print("Digite um número válido!")
 
-n = leiaInt('Digite um número: ')
-print(f'Voce acabou de digitar o número {n}')
+n = leiaInt("Digite um numero: ")
+print(f"Você acabou de digitar o número {n}")
