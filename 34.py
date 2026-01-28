31a31fa8f0c6ac65c08b3236614302f2f9ed1cf9
itzmarcos <marcozfuwa@gmail.com>
2026-01-28 17:12:15 -0300

    att 35

--- 34.py
+++ 34.py
@@ -4,15 +4,14 @@ def notas(*num, sit):
    r['total'] = len(num)
    r['maior'] = max(num)
    r['menor'] = min(num)
-   r['media'] = sum(num)/len(num)
+   r['media'] = sum(num)/len(num)  
+   if sit:
+      if r['media'] >= 7:
+         r['situaçao'] = 'BOA'
+      elif r['media'] >= 5:
+         r['situaçao'] = 'RAZOAVEL'
+      else:
+         r['situaçao'] = 'RUIM'
    return r
-#    if sit:
-#       if r['media'] >= 7:
-#          r['situaçao'] = 'BOA'
-#       elif r['media'] >= 5:
-#          r['situaçao'] = 'RAZOAVEL'
-#       else:
-#          r['situaçao'] = 'RUIM'
-#     return r
 resp = notas(5, 6, 5, sit=True)
 print(resp)
--- 35.py
+++ 35.py
@@ -1,17 +1,30 @@
 #
-def lin(f):
-    tam = len(f)
-    print('-=' * tam)
+from time import sleep
+c = ('\033[m',
+     '\033[0;30;41m', # 0 - sem cores
+     '\33[0;30;42m',  # 1 - red
+     '\33[0;30;43m',  # 2 - green
+     '\33[0;30;43m',  # 3 - yellow
+     '\33[0;30;44',)  # 4 - blue
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'', 4)
+    help(com)
+    sleep(1)
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+
+comando = ''
 while True:
-    lin()
-    print('SISTEMA DE AJUDA PyHELP')
-    lin()
-    f = str(input('funcao ou Biblioteca: '))
-    lin()
-    print(f'Acessando o manual {f}')
-    lin()  
-    if f == 'FIM':
+    titulo('SISTEMA DE AJUDA PyHELP')
+    comando = str(input("Funcao ou Biblioteca: "))
+    if comando.upper() == 'FIM':
         break
-help(f)
-lin()
-print('Programa Finalizado..')
+    else:
+        ajuda(comando)
+titulo('ATE LOGO')
